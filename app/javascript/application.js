ced62942403c9848531c3ba457402718ed99259c
caixj <mc02cxj@gmail.com>
2025-10-19 21:29:20 +0800

    ok

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
 import "@hotwired/turbo-rails"
+
+
+import {Goban} from "@sabaki/shudan";
+
+// const layout = Shudan.createLayout({
+//   width: 500,
+//   height: 500,
+//   gridSize: 20,
+// });
+
+// console.log("Shudan 布局初始化完成", layout);
